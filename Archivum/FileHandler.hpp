6ff45a4555db12a25853a0d948cf81e74819addc
GyörgyFerenc <gyferenc2002@gmail.com>
2022-11-21 18:15:25 +0200

    Blank sheet

--- Archivum/FileHandler.hpp
+++
@@ -1,6 +0,0 @@
-#include "MadLibrary.hpp"
-
-bool MadLibrary::CheckForFileExists(const std::string& name){
-    std::ifstream f(name.c_str());
-    return f.good();
-}
--- MadLibrary/MadLibraryAll.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MadLibrary__HPP
-#define MadLibrary__HPP
-
-#include "FileHandler.hpp"
-#include "Graph.hpp"
-#include "Matrix.hpp"
-#include "SimpleFunctions.hpp"
-
-#endif
