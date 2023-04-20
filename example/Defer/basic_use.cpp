67a96e3bc231a3b12678937459f5fd4ecf5a0e4b
GyörgyFerenc <gyferenc2002@gmail.com>
2023-04-20 11:11:41 +0300

    Changed defer

--- example/Defer/basic_use.cpp
+++ example/Defer/basic_use.cpp
@@ -3,17 +3,16 @@
 #include "../../include/Defer.hpp"
 
 int main(int argc, char const* argv[]) {
-    // Initiailizes Defering in this scope
-    DEFER_INIT;
-
     // Will be executed last
-    DEFER(std::cout << "1. Defered" << std::endl);
+    defer {
+        std::cout << "1. Defered" << std::endl;
+    };
 
     // Will be executed before last
-    DEFER({
+    defer {
         std::cout << "2. Defered" << std::endl;
         std::cout << "3. Defered" << std::endl;
-    });
+    };
 
     // Will be executed first
     std::cout << "Not Defered" << std::endl;
--- include/Defer.hpp
+++ include/Defer.hpp
@@ -4,37 +4,30 @@
 #define MADLIBRARY_DEFER
 
 #include <functional>
-#include <stack>
 
-namespace MadLibrary {
-class Defer {
-   public:
-    Defer() {
+struct defer_implementation {
+    std::function<void()> func;
+
+    ~defer_implementation() {
+        func();
     }
-
-    Defer(std::function<void()> fun) {
-        this->add(fun);
-    }
-
-    ~Defer() {
-        while (!stack.empty()) {
-            auto f = stack.top();
-            f();
-            stack.pop();
-        }
-    }
-
-    void add(std::function<void()> fun) {
-        stack.push(fun);
-    }
-
-   private:
-    std::stack<std::function<void()>> stack;
 };
 
-}  // namespace MadLibrary
+struct defer_dummy {
+    defer_implementation operator+(std::function<void()> func) {
+        return defer_implementation{func};
+    }
+};
 
-#define DEFER_INIT auto __def = MadLibrary::Defer()
-#define DEFER(code_block) __def.add([&]() { code_block; })
+#define CONCAT_IMPL(str1, str2) str1##str2
+#define CONCAT(str1, str2) CONCAT_IMPL(str1, str2)
+
+#ifdef __COUNTER__
+#define ANONYMOUS_VARIABLE(str) CONCAT(str, __COUNTER__)
+#else
+#define ANONYMOUS_VARIABLE(str) CONCAT(str, __LINE__)
+#endif
+
+#define defer auto ANONYMOUS_VARIABLE(__defer_instance) = defer_dummy{} + [&]()
 
 #endif
