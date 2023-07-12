7b009006c310771c5c8df012acca4ada8f9e128e
GyörgyFerenc <gyferenc2002@gmail.com>
2023-07-12 12:59:56 +0300

    Worked on TUI

--- include/Core/Queue.hpp
+++ include/Core/Queue.hpp
@@ -1,5 +1,82 @@
 #pragma once
+#include "Context.hpp"
+#include "Intrinsics.hpp"
+#include "Memory.hpp"
 
 // TODO(Ferenc): Implement it
+// rewrite it using list maybe?
+
+/*
+ * Current implementation uses a linked list
+ */
 template <class T>
-struct Queue {};
+struct Queue {
+    struct Node {
+        T     elem;
+        Node* next = nullptr;
+    };
+
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+    usize size = 0;
+
+    Allocator*      allocator;
+    static Queue<T> create(Context context) {
+        Queue<T> q;
+        q.head = nullptr;
+        q.tail = nullptr;
+        q.size = 0;
+        q.allocator = context.allocator;
+        return q;
+    }
+    static Queue<T> create() {
+        return create(default_context());
+    }
+};
+
+template <class T>
+void destroy(Queue<T>& queue) {
+    typename Queue<T>::Node* ptr = queue.head;
+    while (ptr != nullptr) {
+        let tmp = ptr->next;
+        queue.allocator->template free(ptr);
+        ptr = tmp;
+    }
+}
+
+template <class T>
+void enque(Queue<T>& queue, T element) {
+    let try_node = queue.allocator->template allocate<typename Queue<T>::Node>();
+    let node = unwrap(try_node);
+    node->next = nullptr;
+    node->elem = element;
+
+    if (queue.tail != nullptr) queue.tail->next = node;
+    queue.tail = node;
+    if (queue.head == nullptr) queue.head = node;
+
+    queue.size++;
+}
+
+/*
+ * this is does not do boundary checking
+ */
+template <class T>
+T deque(Queue<T>& queue) {
+    queue.size--;
+
+    let elem = queue.head->elem;
+    let tmp = queue.head;
+
+    queue.head = queue.head->next;
+    if (tmp == queue.tail) queue.tail = nullptr;
+    queue.allocator->template free(tmp);
+
+    return elem;
+}
+
+template <class T>
+inline bool empty(Queue<T>& queue) {
+    return queue.size == 0;
+}
--- include/TUI.hpp
+++ include/TUI.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <termios.h>
+
+#include <thread>
+
 #include "Core.hpp"
+#include "Core/CoreErrors.hpp"
 #include "Core/Intrinsics.hpp"
 #include "Core/String.hpp"
 
@@ -59,6 +64,7 @@ struct Canvas {
 
         return canvas;
     }
+
     static Canvas create(usize width, usize height) {
         return create(width, height, default_context());
     }
@@ -75,9 +81,82 @@ void destroy(Canvas& canvas) {
     canvas.allocator->free_array(canvas.matrix, canvas.height);
 }
 
+Error set(Canvas& canvas, usize x, usize y, const char* utf8_chr) {
+    if (y >= canvas.height || x >= canvas.width) return CoreError::OutOfRange;
+    set(canvas[y][x], utf8_chr);
+    return Correct;
+}
+
+Error set(Canvas& canvas, usize x, usize y, const char chr) {
+    if (y >= canvas.height || x >= canvas.width) return CoreError::OutOfRange;
+    set(canvas[y][x], chr);
+    return Correct;
+}
+
+void fill(Canvas& canvas, const char chr) {
+    for (usize i = 0; i < canvas.height; i++) {
+        for (usize j = 0; j < canvas.width; j++) {
+            set(canvas[i][j], chr);
+        }
+    }
+}
+
+Error draw(Canvas& canvas, usize x, usize y, Canvas& to_be_drawn) {
+    if (x + to_be_drawn.width > canvas.width) return CoreError::OutOfRange;
+    if (y + to_be_drawn.height > canvas.height) return CoreError::OutOfRange;
+
+    for (usize i = 0; i < to_be_drawn.height; i++) {
+        for (usize j = 0; j < to_be_drawn.width; j++) {
+            canvas[y + i][x + j] = to_be_drawn[i][j];
+        }
+    }
+
+    return Correct;
+}
+
 struct TUIContext {
-    usize nr_lines_printed = 0;
+    usize          nr_lines_printed = 0;
+    struct termios old_termios_state;
 };
+
+void set_termios_flags(TUIContext& tui_context);
+
+void init(TUIContext& tui_context) {
+    set_termios_flags(tui_context);
+}
+
+void unset_termios_flags(TUIContext& tui_context);
+
+void destroy(TUIContext& tui_context) {
+    unset_termios_flags(tui_context);
+}
+
+/*
+ * Sets the termios flags
+ * so the terminal can be behave
+ * in the expected way
+ */
+void set_termios_flags(TUIContext& tui_context) {
+    if (tcgetattr(0, &tui_context.old_termios_state) < 0)
+        perror("Tset_termios_flags: ermios tcgetattr()");
+    struct termios new_state = tui_context.old_termios_state;
+    new_state.c_lflag &= ~ICANON;
+    new_state.c_lflag &= ~ECHO;
+    new_state.c_lflag &= ~ISIG;
+    new_state.c_cc[VMIN] = 1;
+    new_state.c_cc[VTIME] = 0;
+
+    if (tcsetattr(0, TCSANOW, &new_state) < 0) perror("set_termios_flags: Termios tcsetattr()");
+}
+
+/*
+ * Reset the termios state
+ * for the default one
+ */
+void unset_termios_flags(TUIContext& tui_context) {
+    struct termios old = tui_context.old_termios_state;
+    if (tcsetattr(0, TCSADRAIN, &old) < 0) perror("unset_termios_flags: Termios tcsetattr");
+}
 
 void print_canvas(TUIContext& tui_context, Canvas& canvas) {
     for (usize y = 0; y < canvas.height; y++) {
